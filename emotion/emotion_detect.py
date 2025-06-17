d104d9a1352302c9f1e6d814466014f73b89e8a1
M-D <mohammad.danish.ansari0307@gmail.com>
2025-06-17 21:14:35 +0530

    update in app and req

---
+++ emotion/emotion_detect.py
@@ -0,0 +1,21 @@
+import cv2
+from deepface import DeepFace
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret,img = cap.read()
+
+    results = DeepFace.analyze(img, actions=["emotion"],enforce_detection = False)
+
+    emotion = results[0]['dominant_emotion']
+
+    cv2.putText(img, f'Emotion: {emotion}',(50,50),cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),2)
+
+    resized_img = cv2.resize(img, (480, 360))
+    cv2.imshow("Emotion Recognition",resized_img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
